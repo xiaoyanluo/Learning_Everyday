dae5e8c67c8dfa18b34f1d45898ff6166d1c6798
xiaoyanluo <1096539894@qq.com>
2020-11-05 17:05:00 +0800

    Update one.cpp

--- first/one.cpp
+++ first/one.cpp
@@ -37,3 +37,63 @@ Status ListDelete(Sqlist *L, int i,ElemType *e)
 }
 //以上线性表的顺序结构在 存数据、读数据时的复杂度为O(1). 而插入或删除时为O(n)。
 
+//线性表的单链表存储结构
+type struct Node
+{
+  ElemType data;
+  struct Node *next;
+  
+} Node;
+typedef struct Node *ListList;
+p->next的值是一个指针
+p->next->data表示下一个结点的数据域内的数据
+
+Status GetElem(LinkList L, int i, ElemType *e)
+{
+  int j;
+  LinkList p;
+  p=L->next;//p指向L的第一个结点(L->next 表示第一个结点哦)
+  j=1;//计数器
+  while(p->next && j<i)//p不是NULL，且j还没到i的时候，循环继续
+  {
+    p = p->next;
+    ++j;
+  }
+  if(!p || j>i)
+    return ERROR;
+  *e = p->data;
+  return OK;
+}
+//
+
+//插入数据
+Status ListInsert(LinkList *l,int i, ElemType e){
+  int j;
+  LinkList p,s;
+  p = *L;
+  j = 1;
+  while(p && j<i){
+    p = p->next;
+    ++j;
+  }//寻找第i个结点
+  s = (LinkList)malloc(sizeof(Node));//生成新结点,在内存找到一块空地存放新结点。
+  s->data = e;
+  s->next = p->next;
+  p->next = s;
+  return OK;
+}
+
+//头插法
+void CreatListHead(LinkList *L, int n){
+  LinkList p;
+  int i;
+  *L = (Linklist)malloc(Sizeof(Node));
+  (*L)->next = NULL;//建立一个带头结点的单链表
+   for( i=0;i<n;i++){
+    p=(Linklist)malloc(sizeof(Node));//生成新结点
+     p->data = rand()%100+1;
+     p->next = (*L)->next;
+     (*L)->next = p;//插入到表头
+   }
+}
+
